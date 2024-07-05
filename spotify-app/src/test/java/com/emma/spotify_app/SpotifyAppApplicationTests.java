e9daa2fe9f89a993789953560769b77c75ab55fa
emma wilkinson <emmawilkinsonnxo@gmail.com>
2024-07-05 10:34:35 +0100

    initial commit

---
+++ spotify-app/src/test/java/com/emma/spotify_app/SpotifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma.spotify_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
