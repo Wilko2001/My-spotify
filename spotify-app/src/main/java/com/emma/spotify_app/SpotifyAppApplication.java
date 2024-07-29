77fd4f124eec7ee0dcd13ad15ee1d06ff2f36f63
emma wilkinson <emma.wilkinson2@flutteruki.com>
2024-07-29 13:23:45 +0100

    made a get request for song, and audiofeatures

--- spotify-app/src/main/java/com/emma/spotify_app/SpotifyAppApplication.java
+++ spotify-app/src/main/java/com/emma/spotify_app/SpotifyAppApplication.java
@@ -1,48 +1,24 @@
+// declares the package name for the class
 package com.emma.spotify_app;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
-import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
-import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 import java.io.IOException;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 
 @SpringBootApplication
 public class SpotifyAppApplication {
-    private static final Dotenv dotenv = Dotenv.load();
-    private static final String clientId = dotenv.get("SPOTIFY_CLIENT_ID");
-    private static final String clientSecret = dotenv.get("SPOTIFY_CLIENT_SECRET");
-
-    private static SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId(clientId)
-            .setClientSecret(clientSecret)
-            .build();
-    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
-            .build();
-
-    public static void clientCredentials_Sync() {
-        System.out.println("Starting client credentials sync...");
-        try {
-            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
-
-            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-            System.out.println("Access token: " + clientCredentials.getAccessToken());
-            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-        } catch (IOException | SpotifyWebApiException | ParseException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException, SpotifyWebApiException {
         SpringApplication.run(SpotifyAppApplication.class, args);
-        clientCredentials_Sync();
+    try {
+        System.out.println(new SpotifyController().getSong("5QpjkwvagLUbiqbhD71UCh"));
+    } catch (Exception e) {
+        System.out.println("error: " + e.getMessage());
+        }
+    System.out.println(new SpotifyController().getMood("5QpjkwvagLUbiqbhD71UCh"));
     }
 
 }
